36c2a728226aa8e6c80624cf3cd1b782cb8a82ff
tumaa <ramanfatuu@gmail.com>
2019-09-29 08:45:22 +0300

    call access token to get git hub credentials

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+private  username:string;
+private inform = "c7388d45e8e8b3b9f959d76d178dcdba76e07622";
+private repos: string;
 
-  constructor() { }
+  constructor(private tuma:HttpClient) {
+  console.log ("hello");
+  this.username = "tumaa";
+  
+   }
 }
